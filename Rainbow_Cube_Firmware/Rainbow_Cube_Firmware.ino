0879266b6e43b8e217e5df3b6ccbacbd8bbfaf95
brendan-byrne <byrnework@gmail.com>
2014-04-02 18:23:56 -0400

    Arduino Code Updated

--- Rainbow_Cube_Firmware/Rainbow_Cube_Firmware.ino
+++ Rainbow_Cube_Firmware/Rainbow_Cube_Firmware.ino
@@ -1,1 +1,27 @@
+int cc;
+int ccValue;
+
+int ccArray[] = {20, 21, 22, 30, 31, 32, 40, 41, 42, 50, 51, 52};
+
+void OnControlChange(byte channel, byte control, byte value) {
+  cc = control;
+  ccValue = value;
+}
+
+#include "Tlc5940.h"
+
+void setup() {
+  usbMIDI.setHandleControlChange(OnControlChange);
+  Tlc.init();
+  Tlc.set(0, 4000);
+}
+
+void loop()
+{
+  usbMIDI.read();
+  for (int x=0; x<12; x++) {
+    if (cc == ccArray[x]) Tlc.set(x, ccValue*32);
+  }
+  Tlc.update();
+}
 
